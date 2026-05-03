4696e9085e406f0b1e107e1ef6f48cd364551b86
Suresh <suresh.babu.chinthala99@gmail.com>
2026-05-03 18:59:20 +0530

    updated hooks

--- src/test/java/Utility/hooks.java
+++ src/test/java/Utility/hooks.java
@@ -4,9 +4,8 @@ import io.cucumber.java.Before;
 import io.cucumber.java.After;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 
-public class Hooks {
+public class hooks {
 
     @Before
     public void setDriver() {
